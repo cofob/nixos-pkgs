cb961df1845bc0e0c44deea84d4bd02a2b27a5be
Egor Ternovoy <cofob@riseup.net>
2022-09-27 00:19:34 +0700

    Add tmm

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
   deta = pkgs.callPackage ./pkgs/deta { };
   infrared = pkgs.callPackage ./pkgs/infrared { };
   proxmox-backup = pkgs.callPackage ./pkgs/proxmox-backup { };
+  tmm = pkgs.callPackage ./pkgs/tmm { };
   ultimmc = pkgs.callPackage ./pkgs/ultimmc { };
   woodpecker = pkgs.callPackage ./pkgs/woodpecker { };
 }
