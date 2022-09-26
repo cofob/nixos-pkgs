c53c138000b26c5021599fb5b4d4298ca08a1919
Egor Ternovoy <cofob@riseup.net>
2022-09-27 00:13:18 +0700

    Add woodpecker package

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 {
   deta = pkgs.callPackage ./pkgs/deta { };
-  ultimmc = pkgs.callPackage ./pkgs/ultimmc { };
   infrared = pkgs.callPackage ./pkgs/infrared { };
   proxmox-backup = pkgs.callPackage ./pkgs/proxmox-backup { };
+  ultimmc = pkgs.callPackage ./pkgs/ultimmc { };
+  woodpecker = pkgs.callPackage ./pkgs/woodpecker { };
 }
