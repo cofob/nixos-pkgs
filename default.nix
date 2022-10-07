64be6a904eed96a2524280457d3c31254cc5a907
Egor Ternovoy <cofob@riseup.net>
2022-10-07 19:40:39 +0700

    Remove cockroach alias

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@
 {
   inherit (pkgs.callPackage ./pkgs/cockroachdb { })
     cockroachdb_20_2 cockroachdb_21_1 cockroachdb_21_2;
-  cockroachdb = (pkgs.callPackage ./pkgs/cockroachdb { }).cockroachdb_21_2;
   deta = pkgs.callPackage ./pkgs/deta { };
   infrared = pkgs.callPackage ./pkgs/infrared { };
   proxmox-backup = pkgs.callPackage ./pkgs/proxmox-backup { };
